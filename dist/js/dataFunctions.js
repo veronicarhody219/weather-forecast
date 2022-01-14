890f1dd6993bf7c83b414ea0f83d519ab694a5d1
Veronica-rhody-219 <ngocdiep219@gmail.com>
2022-01-15 06:38:09 +0700

    serverless functions

--- dist/js/dataFunctions.js
+++ dist/js/dataFunctions.js
@@ -14,15 +14,31 @@ export const getHomeLocation = () => {
 };
 
 export const getWeatherFromCoords = async (locationObj) => {
-  const lat = locationObj.getLat();
-  const lon = locationObj.getLon();
-  const units = locationObj.getUnit();
-  const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&units=${units}&appid=${WEATHER_API_KEY}`;
+  // const lat = locationObj.getLat();
+  // const lon = locationObj.getLon();
+  // const units = locationObj.getUnit();
+  // const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&units=${units}&appid=${WEATHER_API_KEY}`;
 
-  // https://api.openweathermap.org/data/2.5/onecall?lat=19&lon=73&exclude=minutely,hourly,alerts&units=metric&appid=faf9aa1b52fc33a0599c634c8ee6035a
+  // // https://api.openweathermap.org/data/2.5/onecall?lat=19&lon=73&exclude=minutely,hourly,alerts&units=metric&appid=faf9aa1b52fc33a0599c634c8ee6035a
 
+  // try {
+  //   const weatherStream = await fetch(url);
+  //   const weatherJson = await weatherStream.json();
+  //   return weatherJson;
+  // } catch (error) {
+  //   console.error(error);
+  // }
+
+  const urlDataObj = {
+    lat: locationObj.getLat(),
+    lon: locationObj.getLon(),
+    units: locationObj.getUnit(),
+  };
   try {
-    const weatherStream = await fetch(url);
+    const weatherStream = await fetch("./.netilify/functions/get_weather", {
+      method: "POST",
+      body: JSON.stringify(urlDataObj),
+    });
     const weatherJson = await weatherStream.json();
     return weatherJson;
   } catch (error) {
@@ -31,19 +47,34 @@ export const getWeatherFromCoords = async (locationObj) => {
 };
 
 export const getCoordsFromApi = async (entryText, units) => {
-  const regex = /^\d+$/g;
-  const flag = regex.test(entryText) ? "zip" : "q";
-  const url = `https://api.openweathermap.org/data/2.5/weather?${flag}=${entryText}&units=${units}&appid=${WEATHER_API_KEY}`;
-  // https://api.openweathermap.org/data/2.5/weather?q=Moscow&units=metric&appid=faf9aa1b52fc33a0599c634c8ee6035a
+  // const regex = /^\d+$/g;
+  // const flag = regex.test(entryText) ? "zip" : "q";
+  // const url = `https://api.openweathermap.org/data/2.5/weather?${flag}=${entryText}&units=${units}&appid=${WEATHER_API_KEY}`;
+  // // https://api.openweathermap.org/data/2.5/weather?q=Moscow&units=metric&appid=faf9aa1b52fc33a0599c634c8ee6035a
 
-  const encodeUrl = encodeURI(url);
+  // const encodeUrl = encodeURI(url);
+  // try {
+  //   const dataStream = await fetch(encodeUrl);
+  //   const jsonData = await dataStream.json();
+
+  //   return jsonData;
+  // } catch (err) {
+  //   console.error(err.stack);
+  // }
+
+  const urlDataObj = {
+    text: entryText,
+    units: units,
+  };
   try {
-    const dataStream = await fetch(encodeUrl);
+    const dataStream = await fetch("./.netilify/functions/get_coords", {
+      method: "POST",
+      body: JSON.stringify(urlDataObj),
+    });
     const jsonData = await dataStream.json();
-    
     return jsonData;
-  } catch (err) {
-    console.error(err.stack);
+  } catch (error) {
+    console.error(error);
   }
 };
 
